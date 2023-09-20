d712958d439c0891093b162e2c4aef6b91b0a9cb
Jacob Moore <jakewymoore@gmail.com>
2023-09-19 21:04:42 -0600

    new menu styles

--- apps/base_website/static/base_website/js/global.js
+++ apps/base_website/static/base_website/js/global.js
@@ -1,2 +1,11 @@
 console.log('test')
 
+function hamMenu() {
+    menu = document.getElementById('menu-popup');
+
+    if (menu.style.height === "100vh") {
+        menu.style.height = "0px";
+    } else {
+        menu.style.height = "100vh";
+    }
+}
